96a42f81661af29d85ccedebf5efd4b3b3f8c996
tunglethanh <90467128+tunglethanh@users.noreply.github.com>
2024-06-04 23:26:37 -0400

    Update Client.cpp

--- MUSES/Client/Client.cpp
+++ MUSES/Client/Client.cpp
@@ -215,6 +215,7 @@ void test_secret_key_update() {
         joinNclean(works);
 
         writer_time += time_from(start);
+		
         cout << "[Permission revocation] Writer latency: " << writer_time << endl;
 		cout << "[Permission revocation] Writer bandwidth: " << nP * bloom_filter_size * (sizeof(private_token_t) + sizeof(poly_modq_t))/(double)1048576.0 << endl;
 		
