7ca956f62c6301e036ea0e2c6f3e6f3e0c7a7240
tunglevt <tungle@vt.edu>
2024-06-09 11:25:10 -0400

    Refactor

--- MUSES/Server/search_index.hpp
+++ MUSES/Server/search_index.hpp
@@ -173,8 +173,8 @@ void init_encrypted_search_index() {
 
         for(int i = 0; i < bloom_filter_size; ++i) 
             delete search_index[wid][i];
-	
-		delete [] search_index[wid];
+	    
+        delete [] search_index[wid];
     }
 
     delete [] search_index;
